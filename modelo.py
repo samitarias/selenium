f2c60ce71644ac78354e7ddc7a9e9847b5e4e9dd
Samit <samitarias@gmail.com>
2021-10-01 19:14:41 -0500

    Segundo Commit#

---
+++ modelo.py
@@ -0,0 +1,14 @@
+import unittest
+from selenium import webdriver
+
+class HomePageTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+
+    unittest.main(verbosity = 2)
